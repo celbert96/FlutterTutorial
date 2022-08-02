09b158580994d7593be30079f1e863bf106d4333
Christopher Elbert <celbert96@gmail.com>
2022-08-02 15:21:14 -0400

    Widget lifecycle fns

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,10 +8,33 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print("Init state fn ran");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Choose Location Screen"),
+    print("Build fn ran");
+    return Scaffold (
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text("Choose Location"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
